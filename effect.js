078f8ccd9f955c366a1c3029f9050269c68b729b
Axololt7923 <168002812+Axololt7923@users.noreply.github.com>
2024-09-15 22:03:46 +0700

    Add files via upload

--- effect.js
+++ effect.js
@@ -1,15 +1,15 @@
-$(window).load(function(){
+$(window).load(function () {
 	$('.loading').fadeOut('fast');
 	$('.container').fadeIn('fast');
 });
-$('document').ready(function(){
-		var vw;
-		$(window).resize(function(){
-			 vw = $(window).width()/2;
-			
-		});
+$('document').ready(function () {
+	var vw;
+	$(window).resize(function () {
+		vw = $(window).width() / 2;
 
-	$('#turn_on').click(function(){
+	});
+
+	$('#turn_on').click(function () {
 		$('#bulb_yellow').addClass('bulb-glow-yellow');
 		$('#bulb_red').addClass('bulb-glow-red');
 		$('#bulb_blue').addClass('bulb-glow-blue');
@@ -17,57 +17,53 @@ $('document').ready(function(){
 		$('#bulb_pink').addClass('bulb-glow-pink');
 		$('#bulb_orange').addClass('bulb-glow-orange');
 		$('body').addClass('peach');
-		$(this).fadeOut('slow').delay(5000).promise().done(function(){
+		$(this).fadeOut('slow').delay(5000).promise().done(function () {
 			$('#play').fadeIn('slow');
 		});
 	});
-	$('#play').click(function(){
+	$('#play').click(function () {
 		var audio = $('.song')[0];
-        audio.play();
-        $('#bulb_yellow').addClass('bulb-glow-yellow-after');
+		audio.play();
+		$('#bulb_yellow').addClass('bulb-glow-yellow-after');
 		$('#bulb_red').addClass('bulb-glow-red-after');
 		$('#bulb_blue').addClass('bulb-glow-blue-after');
 		$('#bulb_green').addClass('bulb-glow-green-after');
 		$('#bulb_pink').addClass('bulb-glow-pink-after');
 		$('#bulb_orange').addClass('bulb-glow-orange-after');
-		$('body').css('backgroud-color','#FFF');
-		$(this).fadeOut('slow').delay(6000).promise().done(function(){
+		$('body').css('backgroud-color', '#FFF');
+		$(this).fadeOut('slow').delay(6000).promise().done(function () {
 			$('#bannar_coming').fadeIn('slow');
 		});
 	});
-
-	$('#bannar_coming').click(function(){
+	$('#bannar_coming').click(function () {
 		$('.bannar').addClass('bannar-come');
-		$(this).fadeOut('slow').delay(6000).promise().done(function(){
+		$(this).fadeOut('slow').delay(6000).promise().done(function () {
 			$('#balloons_flying').fadeIn('slow');
 		});
 		$('.bannar').addClass('bannar-after');
 	});
-
-	$('#balloons_flying').click(function(){
-		$('.balloon-border').animate({top:-500},8000);
-		$(this).fadeOut('slow').delay(5000).promise().done(function(){
+	$('#balloons_flying').click(function () {
+		$('.balloon-border').animate({ top: -500 }, 8000);
+		$(this).fadeOut('slow').delay(5000).promise().done(function () {
 			$('#cake_fadein').fadeIn('slow');
 		});
-	});	
-
-	$('#cake_fadein').click(function(){
+	});
+	$('#cake_fadein').click(function () {
 		$('.cake').fadeIn('slow');
 		$('.overplay').fadeIn('slow');
-		$(this).fadeOut('slow').delay(3000).promise().done(function(){
+		$(this).fadeOut('slow').delay(3000).promise().done(function () {
 			$('#light_candle').fadeIn('slow');
 		});
 	});
-
-	$('#light_candle').click(function(){
+	$('#light_candle').click(function () {
 		$('.light').animate({
-			width: '300px', 
-			height: '300px', 
-			opacity: 0.6    
+			width: '300px',
+			height: '300px',
+			opacity: 0.6
 		}, {
-			duration: 2000, 
-			easing: 'swing', 
-			step: function(now, fx) {
+			duration: 2000,
+			easing: 'swing',
+			step: function (now, fx) {
 				if (fx.prop === 'width') {
 					let radius = now / 2;
 					$(this).css({
@@ -77,49 +73,45 @@ $('document').ready(function(){
 			},
 		});
 		$('.fuego').fadeIn('slow');
-		$(this).fadeOut('slow').promise().done(function(){
+		$(this).fadeOut('slow').promise().done(function () {
 			$('#wish_message').fadeIn('slow');
 		});
 	});
-
-		
-	$('#wish_message').click(function(){
+	$('#wish_message').click(function () {
 		// $('.overplay').fadeOut('slow');
 		// $('.light').fadeOut('slow');
-		$(this).fadeOut('slow').delay(3000).promise().done(function(){
+		$(this).fadeOut('slow').delay(3000).promise().done(function () {
 			$('#story').fadeIn('slow');
 		});
 	});
-	
-	$('#story').click(function(){
+	$('#story').click(function () {
 		$(this).fadeOut('slow');
 		$('.overplay').fadeIn('slow');
-		$('.cake').fadeOut('fast').promise().done(function(){
+		$('.cake').fadeOut('fast').promise().done(function () {
 			$('.message').fadeIn('slow');
 		});
-		
 		var i;
 
-		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
-			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					$('.cake').fadeIn('fast');
-				});
-				
-			}
-			else{
-				msgLoop(i);
-			}			
+		function msgLoop(i) {
+			$("p:nth-child(" + i + ")").fadeOut('slow').delay(800).promise().done(function () {
+				i = i + 1;
+				$("p:nth-child(" + i + ")").fadeIn('slow').delay(1000);
+				if (i == 50) {
+					$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+						$('.cake').fadeIn('fast');
+					});
 
-		});
+				}
+				else {
+					msgLoop(i);
+				}
+
+			});
 			// body...
 		}
-		
+
 		msgLoop(0);
-		
+
 	});
 });
 
